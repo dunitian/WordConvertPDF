ab30bf48a9910fa196e2e466fe978f13700be36f
dunitian <1054186320@qq.com>
2015-12-13 14:34:04 +0800

    批量转换性能优化

--- WordConvertPDF/WordToPDFHelper.cs
+++ WordConvertPDF/WordToPDFHelper.cs
@@ -11,10 +11,10 @@ namespace WordConvertPDF
     public static class WordToPDFHelper
     {
         /// <summary>
-        /// Word转换成PDF
+        /// Word转换成PDF(单个文件转换推荐使用)
         /// </summary>
-        /// <param name="inputPath">载入路径</param>
-        /// <param name="outputPath">保存路径</param>
+        /// <param name="inputPath">载入完整路径</param>
+        /// <param name="outputPath">保存完整路径</param>
         /// <param name="startPage">初始页码（默认为第一页[0]）</param>
         /// <param name="endPage">结束页码（默认为最后一页）</param>
         public static bool WordToPDF(string inputPath, string outputPath, int startPage = 0, int endPage = 0)
@@ -101,10 +101,104 @@ namespace WordConvertPDF
         }
 
         /// <summary>
+        /// Word转换成PDF(批量文件转换推荐使用)
+        /// </summary>
+        /// <param name="inputPath">文件完整路径</param>
+        /// <param name="outputPath">保存路径</param>
+        public  static int WordsToPDFs(string[] inputPaths, string outputPath)
+        {
+            int count = 0;
+
+            #region 初始化
+            //初始化一个application
+            Application wordApplication = new Application();
+            //初始化一个document
+            Document wordDocument = null;
+            #endregion
+
+            //默认值
+            object paramMissing = Type.Missing;
+
+            for (int i = 0; i < inputPaths.Length; i++)
+            {
+                #region 参数设置~~我去累死宝宝了~~（所谓的参数都是根据这个方法来的:ExportAsFixedFormat）
+                //word路径
+                object wordPath = Path.GetFullPath(inputPaths[i]);
+
+                //获取文件名
+                string outputName = Path.GetFileNameWithoutExtension(inputPaths[i]);
+
+                //输出路径
+                string pdfPath = Path.GetFullPath(outputPath + @"\" + outputName + ".pdf");
+
+                //导出格式为PDF
+                WdExportFormat wdExportFormat = WdExportFormat.wdExportFormatPDF;
+
+                //导出大文件
+                WdExportOptimizeFor wdExportOptimizeFor = WdExportOptimizeFor.wdExportOptimizeForPrint;
+
+                //导出整个文档
+                WdExportRange wdExportRange = WdExportRange.wdExportAllDocument;
+
+                //开始页码
+                int startIndex = 0;
+
+                //结束页码
+                int endIndex = 0;
+
+                //导出不带标记的文档（这个可以改）
+                WdExportItem wdExportItem = WdExportItem.wdExportDocumentContent;
+
+                //包含word属性
+                bool includeDocProps = true;
+
+                //导出书签
+                WdExportCreateBookmarks paramCreateBookmarks = WdExportCreateBookmarks.wdExportCreateWordBookmarks;                            
+
+                #endregion
+                
+                #region 转换
+                try
+                {
+                    //打开word
+                    wordDocument = wordApplication.Documents.Open(ref wordPath, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing, ref paramMissing);
+                    //转换成指定格式
+                    if (wordDocument != null)
+                    {
+                        wordDocument.ExportAsFixedFormat(pdfPath, wdExportFormat, false, wdExportOptimizeFor, wdExportRange, startIndex, endIndex, wdExportItem, includeDocProps, true, paramCreateBookmarks, true, true, false, ref paramMissing);
+                    }
+                    count++;
+                }
+                catch (Exception ex)
+                {
+                }
+                finally
+                {
+                    //关闭
+                    if (wordDocument != null)
+                    {
+                        wordDocument.Close(ref paramMissing, ref paramMissing, ref paramMissing);
+                        wordDocument = null;
+                    }
+                }
+            }
+
+            //退出
+            if (wordApplication != null)
+            {
+                wordApplication.Quit(ref paramMissing, ref paramMissing, ref paramMissing);
+                wordApplication = null;
+            }
+            return count;
+                #endregion
+        }
+
+        #region 其他
+        /// <summary>
         /// Word转换成PDF（带日记）
         /// </summary>
-        /// <param name="inputPath">载入路径</param>
-        /// <param name="outputPath">保存路径</param>
+        /// <param name="inputPath">载入完整路径</param>
+        /// <param name="outputPath">保存完整路径</param>
         /// <param name="log">转换日记</param>
         /// <param name="startPage">初始页码（默认为第一页[0]）</param>
         /// <param name="endPage">结束页码（默认为最后一页）</param>
@@ -192,5 +286,6 @@ namespace WordConvertPDF
             }
             #endregion
         }
+        #endregion
     }
 }
--- WordToPDF/MainWindow.xaml.cs
+++ WordToPDF/MainWindow.xaml.cs
@@ -27,6 +27,49 @@ namespace WordToPDF
 
         private void WordBtn_Click(object sender, RoutedEventArgs e)
         {
+            #region OldCode
+            ////打开对话框
+            //Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            ////过滤器
+            //dialog.Filter = "文档类型|*.doc;*.docx";
+            ////允许选多个文件
+            //dialog.Multiselect = true;
+            ////是否打开
+            //var result = dialog.ShowDialog();
+            //if (result == true)
+            //{
+            //    //总计
+            //    int total = 0;
+            //    //保存路径
+            //    string savePath = string.Empty;
+            //    //获得文件
+            //    string[] files = dialog.FileNames;
+            //    System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            //    watch.Start();
+            //    for (int i = 0; i < files.Length; i++)
+            //    {
+            //        savePath = System.IO.Path.GetDirectoryName(files[0]);
+            //        //文件名
+            //        string saveName = System.IO.Path.GetFileNameWithoutExtension(files[i]) + ".pdf";
+            //        //存在就转换
+            //        if (System.IO.File.Exists(files[i]))
+            //        {
+            //            if (WordConvertPDF.WordToPDFHelper.WordToPDF(files[i], savePath + @"\" + saveName))
+            //            {
+            //                total++;
+            //            }
+            //        }
+            //    }
+            //    watch.Stop();
+            //    MessageBox.Show("成功转换了：" + total + " 个文件；耗时：" + watch.Elapsed, "逆天友情提醒");
+            //    if (!string.IsNullOrEmpty(savePath))
+            //    {
+            //        System.Diagnostics.Process.Start(savePath);
+            //    }
+            //}
+            #endregion
+
+            #region NewCode
             //打开对话框
             Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
             //过滤器
@@ -37,32 +80,24 @@ namespace WordToPDF
             var result = dialog.ShowDialog();
             if (result == true)
             {
-                //总计
-                int total = 0;
                 //保存路径
                 string savePath = string.Empty;
                 //获得文件
                 string[] files = dialog.FileNames;
-                for (int i = 0; i < files.Length; i++)
-                {
-                    savePath = System.IO.Path.GetDirectoryName(files[0]);
-                    //文件名
-                    string saveName = System.IO.Path.GetFileNameWithoutExtension(files[i]) + ".pdf";
-                    //存在就转换
-                    if (System.IO.File.Exists(files[i]))
-                    {
-                        if (WordConvertPDF.WordToPDFHelper.WordToPDF(files[i], savePath + @"\" + saveName))
-                        {
-                            total++;
-                        }
-                    }
-                }
-                MessageBox.Show("成功转换了：" + total + " 个文件", "逆天友情提醒");
+
+                savePath = System.IO.Path.GetDirectoryName(files[0]);
+
                 if (!string.IsNullOrEmpty(savePath))
                 {
                     System.Diagnostics.Process.Start(savePath);
                 }
-            }
+                System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+                watch.Start();
+                int total = WordConvertPDF.WordToPDFHelper.WordsToPDFs(files, savePath);
+                watch.Stop();
+                MessageBox.Show("成功转换了：" + total + " 个文件；耗时：" + watch.Elapsed, "逆天友情提醒");
+            } 
+            #endregion
         }
 
         private void PdfBtn_Click(object sender, RoutedEventArgs e)
